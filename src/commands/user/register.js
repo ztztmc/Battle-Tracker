1a45a2dc31933d9df68edf49caed1aecbc8fd511
ztzt <113851739+ztztmc@users.noreply.github.com>
2025-06-05 18:22:35 +0530

    Fix register command

--- src/commands/user/register.js
+++ src/commands/user/register.js
@@ -1,54 +1,89 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const axios = require('axios');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const axios = require("axios");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('register')
-    .setDescription('Register to Battle Tracker.')
-    .addStringOption(option =>
-        option.setName('ign').setDescription('Your Minecraft IGN').setRequired(true)
+    .setName("register")
+    .setDescription("Register to Battle Tracker.")
+    .addStringOption((option) =>
+      option
+        .setName("ign")
+        .setDescription("Your Minecraft IGN")
+        .setRequired(true)
     ),
 
   async execute(interaction) {
     await interaction.deferReply();
     const userId = interaction.user.id;
     const username = interaction.user.username;
-    const ign = interaction.options.getString('ign');
+    const ign = interaction.options.getString("ign");
 
-    if(interaction.channelId == "1375877348464791643") {
-      const uuidRes = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
-      if(uuidRes.data.errorMessage) {
-        const userDoesNotExistEmbed = new EmbedBuilder()
-          .setColor(0xff0000) // Red color
-          .setDescription(`### ${process.env.ICON_BLOCK} **The username you entered does not exist.**\n\nPlease re-check the ign: ${ign}\``)
-        await interaction.editReply({
-          embeds: [userDoesNotExistEmbed],
-        });
-        return;
+    if (interaction.channelId == "1375877348464791643") {
+      let uuid;
+      let uuidRes;
+      try {
+        uuidRes = await axios.get(
+          `https://api.mojang.com/users/profiles/minecraft/${ign}`
+        );
+        uuid = uuidRes.data.id;
+      } catch (err) {
+        if (err.response && err.response.status === 404) {
+          console.log("couldn't find ign:", ign);
+          const userDoesNotExistEmbed = new EmbedBuilder()
+            .setColor(0xff0000) // Red color
+            .setDescription(
+              `### ${process.env.ICON_BLOCK} **The username you entered does not exist.**\n\nPlease re-check the ign: ${ign}\``
+            );
+          await interaction.editReply({
+            embeds: [userDoesNotExistEmbed],
+          });
+          return;
+        } else {
+          const mojangErrorEmbed = new EmbedBuilder()
+            .setColor(0xff0000) // Red color
+            .setDescription(
+              `### ${process.env.ICON_BLOCK} **An error occured.**\n\nThere was a problem connecting to the Mojang API. Please try again later.`
+            );
+          await interaction.editReply({
+            embeds: [mojangErrorEmbed],
+          });
+        }
       }
-      const correctIgn = uuidRes.data.name;
-      const uuid = uuidRes.data.id;
 
-      const hypRes = await axios.get(`https://api.hypixel.net/player?key=${process.env.HYPIXEL_API_KEY}&uuid=${uuid}`);
-      if(!hypRes.data.success) {
+      const correctIgn = uuidRes.data.name;
+
+      const hypRes = await axios.get(
+        `https://api.hypixel.net/player?key=${process.env.HYPIXEL_API_KEY}&uuid=${uuid}`
+      );
+      if (!hypRes.data.success) {
         const hypErrorEmbed = new EmbedBuilder()
           .setColor(0xff0000) // Red color
-          .setDescription(`### ${process.env.ICON_BLOCK} **An unexpected error occured.!**\n\nThere was an error getting your information from the Hypixel API.`)
-          .setFooter({ text: 'This error has been reported. Please try again later.' });
+          .setDescription(
+            `### ${process.env.ICON_BLOCK} **An unexpected error occured.!**\n\nThere was an error getting your information from the Hypixel API.`
+          )
+          .setFooter({
+            text: "This error has been reported. Please try again later.",
+          });
         await interaction.editReply({
           embeds: [hypErrorEmbed],
         });
         return;
       }
       const hypDisc = hypRes.data.player.socialMedia.links.DISCORD;
-      await interaction.editReply(`register test\nyour disc: ${username} ign u entered: ${ign} mojang ign: ${correctIgn} disc in hyp: ${hypDisc} match: ${correctIgn == hypDisc}`);
+      await interaction.editReply(
+        `register test\nyour disc: ${username} ign u entered: ${ign} mojang ign: ${correctIgn} disc in hyp: ${hypDisc} match: ${
+          hypDisc?.toLowerCase() === username.toLowerCase()
+        }`
+      );
     } else {
       const wrongChannelEmbed = new EmbedBuilder()
         .setColor(0xff0000) // Red color
-        .setDescription(`### ${process.env.ICON_BLOCK} **You cannot register here!**\n\nUse </register:1373217367433285705> in <#1375877348464791643>`)
+        .setDescription(
+          `### ${process.env.ICON_BLOCK} **You cannot register here!**\n\nUse </register:1373217367433285705> in <#1375877348464791643>`
+        );
       await interaction.editReply({
         embeds: [wrongChannelEmbed],
       });
     }
-  }
+  },
 };
